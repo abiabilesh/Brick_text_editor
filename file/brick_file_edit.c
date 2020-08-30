74a58165a50e3d985a450f2090bfc375b20ed0b8
abiabilesh <42320772+abiabilesh@users.noreply.github.com>
2020-08-30 22:18:07 +0530

    [brick_file_edit]:Added Delete key response
    This commit includes the following features
    * Added code rework in brick_file_init
    * Added Delete key response based VIM delete on insert mode
    
    Note
    * The feature is not yet completely implemented still needs to
      add normal delete mode.

--- file/brick_file_edit.c
+++ file/brick_file_edit.c
@@ -61,21 +61,90 @@ void container_insert_character(struct brick_win_size *win, int character)
         container_add_row(win,"",0);
 }
 
+void process_delete_key_line_append(row_container *container, int row, int col, int data_row)
+{
+    char *data = container[row].data;
+
+    //check if the current line is empty
+    
+    if(container[row].size  == 1){  //just a null terminator          
+        
+        //check whether the next line is empty
+        if(container[row + 1].size == 0){
+            goto process_delete_next_line; 
+        }
+        else{
+            
+            char *next_data = container[row + 1].data;
+            data = (char*) realloc (data, (container[row + 1].size) * sizeof(char*));
+            memcpy(data, next_data, container[row+1].size);
+            container[row].data = data;
+            container[row].size = container[row+1].size;
+
+        }
+    }
+    else{
+
+        //check whether the next line is empty
+        if(container[row + 1].size == 1){    
+            goto process_delete_next_line;
+        }else{
+        
+                char *next_data = container[row + 1].data;
+                data = (char*) realloc (data, (container[row].size + container[row + 1].size - 1) * sizeof(char*));
+                memcpy(&data[container[row].size - 1], next_data, container[row + 1].size);
+                container[row].data = data;
+                container[row].size = strlen(data) + 1;
+
+        }             
+    }    
+    
+    process_delete_next_line:
+        //removing the next line and patching the containers
+        
+        for(int index = row + 1; index < data_row - 1; index++ )
+        {
+            container[index] = container[index + 1];
+        }
+
+        container = realloc (container, sizeof(row_container) * (data_row -1));
+        
+
+}
+
+void process_delete_key_eof(row_container *container, int row, int col, int data_row)
+{
+    char *data = container[row].data;
+
+    //Check whether should we delete inline leftwards
+
+    if(col >0 && (row < data_row)){
+        
+        data = (char*) realloc (data, ((container[row].size) - 1) * sizeof(char*));
+        memcpy(data,data,(container[row].size) - 1);
+        data[container[row].size - 2] = '\0';
+        free(data[container[row].size - 1]);
+        
+        container[row].data = data;
+        container[row].size = strlen(data) + 1; //to include the null terminator
+    
+    }
+    else if(col == 0 && row > 0){
+    
+        free(container[row].data);
+        container[row].size = NULL;
+        //win->container = realloc(win->container, sizeof(row_container) * (win->data_row -1)); ?????
+        
+    }
+}
+
 
 void process_delete_key(struct brick_win_size *win)
 {
-    uint8_t insert_flag = 0;
     int row = win->current_row;
     int col = win->current_column;
     row_container *container = win->container;
     char *data = container[row].data;
-
-/*
-    FILE *fp;
-    fp=fopen("log.txt","a+");
-
-    fprintf(fp,"begin: row:%d, column:%d, row_size:%d, data_row:%d, data:%s\n", row, col, container[row].size, win->data_row, container[row].data);
-*/
 
     /*************************************************************
      NOTE: container[row].size represents the total element in an 
@@ -88,10 +157,9 @@ void process_delete_key(struct brick_win_size *win)
     //check whether should we delete inline
     
     if((col < (container[row].size - 1)) && (row < win->data_row )){ 
-        //fprintf(fp,"we delete inline\n");
+       
         data = (char*) realloc (data, ((container[row].size) - 1) * sizeof(char*));
         memmove(&data[col], &data[col+1], container[row].size - (col + 1));
-        //fprintf(fp,"data:%s size:%d\n",data,strlen(data)+1);
         container[row].data = data;
         container[row].size = strlen(data) + 1; //to include the null terminator
     }
@@ -99,96 +167,35 @@ void process_delete_key(struct brick_win_size *win)
     //check whether should we delete next line and append, also make sure it is not the last line
     
     else if((col == container[row].size - 1) && (row < win->data_row - 1)){
-        //fprintf(fp,"---check whether should we delete next line and append, also make sure it is not the last line--\n");
         
-        //check if the current line is empty
-        
-        if(container[row].size  == 1){  //just a null terminator          
-            //fprintf(fp,"---the current line is empty--\n");
-            
-            //check whether the next line is empty
-            if(container[row + 1].size == 0){
-                //fprintf(fp,"---the next line is empty--\n");
-                goto process_delete_next_line;
-            }else{
-                //fprintf(fp,"---the next line is not empty--\n");
-                char *next_data = container[row + 1].data;
-                data = (char*) realloc (data, (container[row + 1].size) * sizeof(char*));
-                memcpy(data, next_data, container[row+1].size);
-                container[row].data = data;
-                container[row].size = container[row+1].size;
-            }
-        }else{
-            //fprintf(fp,"---current line is not empty--\n");
-            //check whether the next line is empty
-            if(container[row + 1].size == 1){
-                //fprintf(fp,"---the next line is empty--\n");
-                goto process_delete_next_line;
-            }else{
-                 //fprintf(fp,"---the next line is not empty--\n");
-                 char *next_data = container[row + 1].data;
-                 data = (char*) realloc (data, (container[row].size + container[row + 1].size - 1) * sizeof(char*));
-                 memcpy(&data[container[row].size - 1], next_data, container[row + 1].size);
-                 container[row].data = data;
-                 container[row].size = strlen(data) + 1;
-            }             
-        }    
-        
-        process_delete_next_line:
-            //removing the next line and patching the containers
-            for(int index = row + 1; index < win->data_row - 1; index++ )
-            {
-                container[index] = container[index + 1];
-            }
-            container = realloc (container, sizeof(row_container) * (win->data_row -1));
-            win->container = container;
-            win->data_row --;
-            //fprintf(fp, "adding new row size %d and data %s\n", strlen(data)+1, data);
+        process_delete_key_line_append(container, row, col, win->data_row);
+
+        win->container = container;
+        win->data_row --;
     }
  
     //We have reached the end of a file for switch direction and start deleting in opposite way.
     else if(row == win->data_row - 1)
     {
-        //fprintf(fp,"---reached the end of a file--\n");
 
-        //Check whether should we delete inline leftwards
-        if(col >0 && (row < win->data_row)){
-            //fprintf(fp,"we delete inline leftwards\n");
-        
-            data = (char*) realloc (data, ((container[row].size) - 1) * sizeof(char*));
-            memcpy(data,data,(container[row].size) - 1);
-            data[container[row].size - 2] = '\0';
-            free(data[container[row].size - 1]);
-            
+        process_delete_key_eof(container,row, col, win->data_row);
+
+        if(col >0 && (row < win->data_row))
+        {
             //Moving the cursor left
             win->current_column --;
-            //fprintf(fp,"data:%s size:%d\n",data,strlen(data)+1);
-            container[row].data = data;
-            container[row].size = strlen(data) + 1; //to include the null terminator
-        
         }
-        else if(col == 0 && row > 0){
-            //fprintf(fp,"go one line up since the current line is empty\n");
-        
-            free(container[row].data);
-            container[row].size = NULL;
-
-            //win->container = realloc(win->container, sizeof(row_container) * (win->data_row -1)); ?????
-
+        else if(col == 0 && row > 0)
+        {
             //Move the cursor backwards
             win->data_row --;
             win->current_row--;
-		        if(win->container[win->current_row].size != 0)
-                {
-                    win->current_column = win->container[win->current_row].size - 1;
-                }
-
-           
+            
+            if(win->container[win->current_row].size != 0)
+            {
+                win->current_column = win->container[win->current_row].size - 1;
+            }
         }
-        
     }
-
-    //fprintf(fp,"end  : row:%d, column:%d, row_size:%d, data_row:%d, data:%s\n", row, col, container[row].size, win->data_row, container[row].data);
-    //fclose(fp);
 }
 
