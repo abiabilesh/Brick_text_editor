318056ff0bdb0dd4452a2a963197d3a98bde2e02
aul1cob <aul1cob@cob1083324.cob.apac.bosch.com>
2020-09-04 16:37:19 +0530

    [restructure]: Setted up an initial branch

--- include/brick_sys.h
+++
@@ -1,3 +0,0 @@
-
-int brick_get_window_size(int *rows, int *cols);
-int brick_ioctl_fallback(int *rows, int *cols);
--- include/brick_termios.h
+++
@@ -1,4 +0,0 @@
-
-void terminos_raw_clear(void);
-void terminos_raw(Brick *brick);
-void get_terminos_property(Brick *brick);
